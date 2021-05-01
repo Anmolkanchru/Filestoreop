eb001f4e40e31a40395ccca9407562d9c3c0b76c
Gowtham Kumar <72903548+KGK06@users.noreply.github.com>
2021-05-01 14:42:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -133,8 +133,7 @@ async def main(bot, message):
                 chat_id=Config.LOG_CHANNEL,
                 f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!\n\n [File Link](share_link) || [Source](source_link)",
                 parse_mode="Markdown",
-                disable_web_page_preview=True
-                )
+                disable_web_page_preview=True)
             share_link = f"https://t.me/{Config.BOT_USERNAME}?start=TeluguMoviesDL_{file_er_id}"
             await editable.edit(
                 f"**Your File Stored in my Database!**\n\nHere is the Permanent Link of your file: {share_link} \n\nJust Click the link to get your file!",
