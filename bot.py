2ee9f9e6289c038868cef821dab9ce2070937b62
Gowtham Kumar <72903548+KGK06@users.noreply.github.com>
2021-05-01 14:33:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -130,7 +130,7 @@ async def main(bot, message):
             source_link = f"https://t.me/c/{Config.LOG_CHNL}/{file_er_id}"
             share_link = f"https://t.me/{Config.BOT_USERNAME}?start=TeluguMoviesDL_{file_er_id}"
             await bot.send_message (
-                Config.LOG_CHANNEL
+                chat_id=Config.LOG_CHANNEL,
                 f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!\n\n [File Link](share_link) || [Source](source_link)",
                 parse_mode="Markdown", disable_web_page_preview=True)
             share_link = f"https://t.me/{Config.BOT_USERNAME}?start=TeluguMoviesDL_{file_er_id}"
